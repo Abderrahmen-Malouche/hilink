705ac004de223a3efc9ef1b2bc359b9bf60e99f2
Abderrahmen-Malouche <121557323+Abderrahmen-Malouche@users.noreply.github.com>
2024-07-01 20:28:09 +0200

    finished the whole project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Camp from "@/Components/Camp";
+import Features from "@/Components/Features";
+import Getapp from "@/Components/Getapp";
+import Guide from "@/Components/Guide";
+import Hero from "@/Components/Hero";
 
 
 export default function Home() {
   return (
-    <div className="font-bold">
-      Hello world
-    </div>
+    <>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <Getapp/>
+    </>
   );
 }
